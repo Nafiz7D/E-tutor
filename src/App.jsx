529ed2d94dff1e7866fd4935019be9c2a3fd806a
Nafiz7D <mrnafsu07@gmail.com>
2024-09-27 21:56:12 +0600

    added numbercount section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
 import NavbarBanner from "./components/Navbar/NavbarBanner"
+import NumberCounter from "./components/NumberCounter/NumberCounter"
 
 
 const App = () => {
   return (
     <main className="overflow-x-hidden max-w-6xl mx-auto">
-      <Navbar></Navbar>
-      <NavbarBanner></NavbarBanner>
-      <Hero></Hero>
+      <Navbar></Navbar>,
+      <NavbarBanner></NavbarBanner>,
+      <Hero></Hero>,
+      <NumberCounter></NumberCounter>
     </main>
   )
 }
